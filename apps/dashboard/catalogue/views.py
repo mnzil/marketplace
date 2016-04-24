5aa5dc609510d154ea1655a554abeec54a7652ff
Ahmed Shaher <a.shaher16@gmail.com>
2016-04-24 08:32:57 +0200

    Partner Can't see other partners products or update/delete them Add/ Update/ Remove Products

--- apps/dashboard/catalogue/views.py
+++ apps/dashboard/catalogue/views.py
@@ -1,13 +1,41 @@
-from oscar.apps.dashboard.catalogue.views import ProductListView as CoreProductListView
-from oscar.apps.dashboard.catalogue.views import ProductCreateRedirectView as CoreProductCreateRedirectView
+from django.http import HttpResponseRedirect
+from oscar.apps.dashboard.catalogue.views import \
+    ProductListView as CoreProductListView, \
+    ProductCreateRedirectView as CoreProductCreateRedirectView,  \
+    ProductClassCreateUpdateView as CoreProductClassCreateUpdateView, \
+    ProductClassCreateView as CoreProductClassCreateView, \
+    ProductClassListView as CoreProductClassListView, \
+    ProductCreateUpdateView as CoreProductCreateUpdateView, \
+    ProductDeleteView as CoreProductDeleteView
+from oscar.core.loading import get_model
+
+Product = get_model('catalogue', 'Product')
+
+
+def filter_products(queryset, user):
+    shop_id = user.partners.all().values_list(
+        'shop_id',
+        flat=True)[0]
+    queryset = queryset.filter(product_class__shop_id=shop_id).distinct()
+    return queryset
 
 
 class ProductListView(CoreProductListView):
     def get_context_data(self, **kwargs):
         ctx = super(ProductListView, self).get_context_data(**kwargs)
         ctx['form'] = self.form
-        ctx['new_productclass_form'] = self.productclass_form_class(request=self.request)
+        ctx['new_productclass_form'] = self.productclass_form_class(
+            request=self.request)
         return ctx
+
+    def filter_queryset(self, queryset):
+        return filter_products(queryset, self.request.user)
+
+
+class ProductClassListView(CoreProductClassListView):
+    def get_queryset(self):
+        return super(ProductClassListView, self).get_queryset().filter(
+            shop_id=self.request.shop_id)
 
 
 class ProductCreateRedirectView(CoreProductCreateRedirectView):
@@ -19,3 +47,29 @@ class ProductCreateRedirectView(CoreProductCreateRedirectView):
 
         else:
             return self.get_invalid_product_class_url()
+
+
+class ProductClassCreateUpdateView(CoreProductClassCreateUpdateView):
+    def forms_valid(self, form, attributes_formset):
+        up_form = form.save(commit=False)
+        up_form.shop_id = self.request.shop_id
+        up_form.save()
+        attributes_formset.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class ProductClassCreateView(
+        ProductClassCreateUpdateView,
+        CoreProductClassCreateView):
+    pass
+
+
+class ProductCreateUpdateView(CoreProductCreateUpdateView):
+    def get_queryset(self):
+        return filter_products(Product.objects.all(), self.request.user)
+
+
+class ProductDeleteView(CoreProductDeleteView):
+    def get_queryset(self):
+        return filter_products(Product.objects.all(), self.request.user)
