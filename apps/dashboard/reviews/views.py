2fc682a1fbae3b9af243fc1208e717d922f952f8
Ahmed Shaher <a.shaher16@gmail.com>
2016-05-11 15:59:38 +0200

    Removed unnecessary print caused memory error

--- apps/dashboard/reviews/views.py
+++ apps/dashboard/reviews/views.py
@@ -3,8 +3,5 @@ from oscar.apps.dashboard.reviews.views import ReviewListView as CoreReviewListV
 
 class ReviewListView(CoreReviewListView):
     def get_queryset(self):
-        print super(ReviewListView, self).get_queryset().filter(
-            product__product_class__shop_id=self.request.shop_id)
-
         return super(ReviewListView, self).get_queryset().filter(
             product__product_class__shop_id=self.request.shop_id)
